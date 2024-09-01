0cb0eade45e2057f347e1ea5bb97e434b2f39493
Louis Martin du Nord <l.martindunord@gmail.com>
2024-09-01 15:57:24 +0200

    avancée sur data visualisation

---
+++ sql/select_all_datas_for_user.sql
@@ -0,0 +1,29 @@
+-- Usefull to get all the data for the data visualisation 
+
+SELECT 
+    sport_rows.date_seance,
+    sport_rows.sport, 
+    sport_rows.exercice, 
+    sport_rows.duree,
+    NULL AS seance, 
+    NULL AS poid,
+    NULL AS nombre_repetition
+FROM 
+    sport_rows
+WHERE 
+    sport_rows.user_id = %s
+
+UNION ALL 
+
+SELECT 
+    musculation_rows.date_seance,
+    NULL AS sport, 
+    musculation_rows.exercice, 
+    NULL AS duree,
+    musculation_rows.seance,
+    musculation_rows.poid,
+    musculation_rows.nombre_repetition
+FROM 
+    musculation_rows
+WHERE 
+    musculation_rows.user_id = %s;
